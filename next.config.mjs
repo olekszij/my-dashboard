380a94b1ee6215ef4e9cd559322670999fb1f85f
Olekszij <olekszijk@gmail.com>
2025-04-28 01:12:25 +0200

    clean up package.json scripts

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [], // Add any external image domains here
+  },
+};
+
+export default nextConfig; 
